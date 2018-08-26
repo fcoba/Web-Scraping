d35cdfb67ce98401b0eb3b9fd33cc19604cacb11
Filis Coba <filiscoba1@gmail.com>
2018-08-25 21:51:33 -0400

    First completed draft of the hw

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify, redirect
+import pymongo
+import scrape_mars
+
+app = Flask(__name__)
+
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_db
+collection = db.mars_facts
+@app.route('/scrape')
+def scrape():
+    mars = scrape_mars.scrape()
+    # print("\n\n\n")
+    print(mars['featured_image_url'])
+    collection.insert_one(mars)
+    return redirect("http://localhost:5000/", code=302)
+
+@app.route("/")
+def home():
+    mars = list(db.mars_facts.find())
+    print(mars[0]['featured_image_url'])
+    return render_template("index.html", mars = mars[-1])
+
+if __name__ == "__main__":
+    app.run(debug=True)
