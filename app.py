484a45d840f4281f42769556dffd2145117891ab
Filis Coba <filiscoba1@gmail.com>
2018-10-10 09:39:13 -0400

    save revertted stuff

--- app.py
+++ app.py
@@ -1,53 +1,41 @@
 from flask import Flask, render_template, jsonify, redirect
 import pymongo
 import scrape_mars
+<<<<<<< HEAD
 import os
 from flask_pymongo import PyMongo
 
 MONGO_URL = os.environ.get('MONGODB_URI')
 if not MONGO_URL:
     MONGO_URL = "mongodb://localhost:27017/rest"
+=======
+>>>>>>> parent of d7d53ad... Update app.py to use Flask-PyMongo; those changes plus Procfile and requirements needed to deploy to Heroku
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = MONGO_URL
-# conn = 'mongodb://localhost:27017'
-# client = pymongo.MongoClient(conn)
-mongo = PyMongo(app)
-
-# db = client.mars_db
-# collection = db.mars_facts
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_db
+collection = db.mars_facts
 @app.route('/scrape')
 def scrape():
     mars = scrape_mars.scrape()
     # print("\n\n\n")
     print(mars['featured_image_url'])
+<<<<<<< HEAD
     # collection.insert_one(mars)
     mongo.db.mars_facts.insert(mars)
     return redirect("/", code=302)
+=======
+    collection.insert_one(mars)
+    return redirect("http://localhost:5000/", code=302)
+>>>>>>> parent of d7d53ad... Update app.py to use Flask-PyMongo; those changes plus Procfile and requirements needed to deploy to Heroku
 
 @app.route("/")
 def home():
-    # mars = list(db.mars_facts.find())
-    mars = list(mongo.db.mars_facts.find())
-    print(mars) # mars = []
-    # print(mars[0]['featured_image_url'])
-    if (len(mars) > 0):
-        return render_template("index.html", mars = mars[-1])
-    else:
-        return render_template("index.html", mars={
-            'news_title': '',
-            'news_paragraph': '',
-            'featured_image_url': '',
-            'mars_weather': '',
-            'mars_facts_table': '',
-            'mars_hemispheres': [
-                { 'title': '', 'img_url': '' },
-                { 'title': '', 'img_url': '' },
-                { 'title': '', 'img_url': '' },
-                { 'title': '', 'img_url': '' }
-            ]
-        })
+    mars = list(db.mars_facts.find())
+    print(mars[0]['featured_image_url'])
+    return render_template("index.html", mars = mars[-1])
 
 if __name__ == "__main__":
     app.run(debug=True)
